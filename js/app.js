faad4ade285c28d3397290a061ade0b12b0fcbcb
Kenny Poncio <kenpoc4@gmail.com>
2021-03-18 00:20:26 -0600

    Hasta prototypes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-// Objeto literal
+/**--------------------/*
+ * Alterar select
+ /**--------------------*/
+var menuLista = document.querySelector('#menu-prueba');
 
+
+
+/**--------------------/*
+ * Objeto Literal
+ /**--------------------*/
 const persona = {
     nombre: 'Bruno',
     profesion: 'Es un gato',
@@ -7,7 +15,9 @@ const persona = {
 }
 // console.log(persona);
 
-// Objeto constructor
+/**--------------------/*
+ * Objeto constructor
+ /**--------------------*/
 
 function Persona(nombre, profesion, edad) {
 
@@ -16,5 +26,23 @@ function Persona(nombre, profesion, edad) {
     this.edad = edad;
 }
 
-const personaCreada1 = new Persona('Lizzie', 'También es un gato', '3 años');
+// const personaCreada1 = new Persona('Lizzie', 'También es un gato', '3 años');
 // console.log(personaCreada1);
+
+/**--------------------/*
+ * Prototype
+ /**--------------------*/
+
+ 
+function Tarea(nombre, urgencia) {
+    this.nombre = nombre;
+    this.urgencia   = urgencia;
+}
+    
+Tarea.prototype.mostrarInformacionTarea = function() {
+    return `La tarea ${this.nombre} tiene una prioridad de ${this.urgencia}`;
+}
+
+const tarea1 = new Tarea('Aprender javascript y react', 'Urgente');
+console.log(tarea1);
+console.log(tarea1.mostrarInformacionTarea());
